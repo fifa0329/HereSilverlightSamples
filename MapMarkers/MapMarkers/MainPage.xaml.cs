43e80fbb3c44bafec7c62346583dc2e87eef79be
lingxiang.wang <lingxiang.wang@autonavi.com>
2014-11-11 16:00:13 +0800

    clean code

--- MapMarkers/MapMarkers/MainPage.xaml.cs
+++ MapMarkers/MapMarkers/MainPage.xaml.cs
@@ -1,22 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-using MapMarkers.Resources;
 using Microsoft.Phone.Maps.Controls;
 using System.Diagnostics;
-using System.Windows.Controls.Primitives;
 using System.Device.Location;
-using System.Collections.ObjectModel;
 
 
 namespace MapMarkers
--- MapMarkers/MapMarkers/Resources/AppResources.Designer.cs
+++ MapMarkers/MapMarkers/Resources/AppResources.Designer.cs
@@ -10,9 +10,6 @@
 
 namespace MapMarkers.Resources
 {
-    using System;
-
-
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
